01571de1f1ae2da837e9fa154963eee8017f87d8
desiokta <desiokt10@gmail.com>
2021-11-01 09:32:02 +0700

    Healthful isi

---
+++ app/src/main/java/com/example/healthfull/artikel1.java
@@ -0,0 +1,14 @@
+package com.example.healthfull;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class artikel1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artikel1);
+    }
+}
---
+++ app/src/main/java/com/example/healthfull/video1.java
@@ -0,0 +1,14 @@
+package com.example.healthfull;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class video1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video1);
+    }
+}
